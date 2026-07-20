52cbeddf01e8d043a2c7570d293131d68c9d7e62
Ahmed Hikal <ahmedhikal2002@gmail.com>
2026-07-20 09:24:19 +0300

    Add laboratory

---
+++ MEDSYstemITI-solution/MEDSYstemITI/Application/DTOs/Laboratory/TechnicianBriefDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Laboratory
+{
+    public class TechnicianBriefDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Contact { get; set; }
+       
+    }
+}
