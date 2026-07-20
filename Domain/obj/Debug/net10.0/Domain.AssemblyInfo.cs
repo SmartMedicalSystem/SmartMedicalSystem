27016883b98583e3801b2970c5828fe82b782326
mohaed tarek <fcb6480@gmail.com>
2026-07-20 20:01:44 +0300

    Update AssemblyInformationalVersionAttribute hashes
    Updated the AssemblyInformationalVersionAttribute in both Application.AssemblyInfo.cs and Domain.AssemblyInfo.cs to reflect the new source version hash 066e27bd7212a86ad4d89ff9aa1f7ae261636117, replacing the previous hash.

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e0247f458b370b01e0b4b624012590e4c7d1f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+066e27bd7212a86ad4d89ff9aa1f7ae261636117")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
