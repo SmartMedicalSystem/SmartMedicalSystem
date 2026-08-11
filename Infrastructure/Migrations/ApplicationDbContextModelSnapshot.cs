6d9158e003db95a0ef79c8ab9a6116d39747b4c6
mohaed tarek <fcb6480@gmail.com>
2026-08-11 15:56:26 +0300

    Update AdminPersons schema and remote DB connection
    Updated ApplicationDbContextModelSnapshot to set AdminPersons table schema to null. Changed DefaultConnection in appsettings.json to use new remote SQL Server credentials.

--- Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -901,7 +901,7 @@ namespace Infrastructure.Migrations
                 {
                     b.HasBaseType("Domain.Entities.Person.BasePerson");
 
-                    b.ToTable("AdminPersons");
+                    b.ToTable("AdminPersons", (string)null);
                 });
 
             modelBuilder.Entity("Domain.Entities.Doctor", b =>
